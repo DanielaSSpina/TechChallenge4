28a17fa59ba4d539aa77b95c98ffe86771b09bbf
DanielaSSpina <46855272+DanielaSSpina@users.noreply.github.com>
2024-10-27 20:33:30 -0300

    Update Introdução.py

--- pages/Introdução.py
+++ pages/Introdução.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 def app():
     # Exibir a imagem como cabeçalho
-    st.image("https://github.com/seu-usuario/meu-repositorio/raw/main/assets/imagem.jpg")  # ou a URL da imagem
+    st.image("https://github.com/DanielaSSpina/TechChallenge4/blob/main/imagensapoio/Gemini_Generated_P%C3%A1gina%20inicial.jpeg")  # ou a URL da imagem
     
     st.title("Introdução ao trabalho.")
     st.write("Bem-vindo à página inicial do app!")
