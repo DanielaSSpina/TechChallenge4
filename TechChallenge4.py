f4d4a3f8106a03a3fad04ed8f5e66f78ada1a4e5
DanielaSSpina <46855272+DanielaSSpina@users.noreply.github.com>
2024-10-27 19:04:39 -0300

    Update TechChallenge4.py

--- TechChallenge4.py
+++ TechChallenge4.py
@@ -1,3 +1,22 @@
 import streamlit as st
+from Pages.Home import app as home_app
+from Pages.About import app as about_app
 
-st.title('Teste')
+st.title('Tech Challenge 4 | Fiapp')
+
+# Dicionário com as páginas
+pages = {
+    "Introdução": Introdução_app,
+    "Metodologia": Metodologia_app,
+    "Análise": Análise_app,
+    "Previsão": Previsão_app,
+    "Referências": Referências_app,
+}
+
+# Menu lateral de navegação
+st.sidebar.title("Menu")
+selection = st.sidebar.radio("Ir para", list(pages.keys()))
+
+# Carregar a página selecionada
+page = pages[selection]
+page()
