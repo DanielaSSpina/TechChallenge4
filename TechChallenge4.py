99bafbb30682e50ebe48f1108dd0560fddfcf707
DanielaSSpina <46855272+DanielaSSpina@users.noreply.github.com>
2024-10-27 20:36:22 -0300

    Update TechChallenge4.py

--- TechChallenge4.py
+++ TechChallenge4.py
@@ -16,3 +16,10 @@ pages = {
     "Referências": Referências_app
 }
 
+# Menu de navegação
+st.sidebar.title("Menu")
+selection = st.sidebar.radio("Ir para", list(pages.keys()))
+
+# Carregar a página selecionada
+page = pages[selection]
+page()
