ab02e6765e106e2d21a0ada27d32cb4cae061564
DanielaSSpina <46855272+DanielaSSpina@users.noreply.github.com>
2024-10-27 22:21:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
 import streamlit as st
-from pages.Introdução import app as Introdução_app
-from pages.Metodologia import app as Metodologia_app
-from pages.Análise import app as Análise_app
-from pages.Previsão import app as Previsão_app
-from pages.Referências import app as Referências_app
 
 st.title('Tech Challenge 4 | Fiapp')
 st.subheader('teste')
+st.write('agora vai funcioanr essa porra do caralho')
