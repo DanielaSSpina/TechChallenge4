fb5f23f383813c8263fe67594d780e07d6b487a1
DanielaSSpina <46855272+DanielaSSpina@users.noreply.github.com>
2024-10-27 22:21:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,32 +7,3 @@ from pages.Referências import app as Referências_app
 
 st.title('Tech Challenge 4 | Fiapp')
 st.subheader('teste')
-
-# Dicionário com as páginas
-pages = {
-    "Introdução": Introdução_app,
-    "Metodologia": Metodologia_app,
-    "Análise": Análise_app,
-    "Previsão": Previsão_app,
-    "Referências": Referências_app
-}
-
-# Inicializa a variável para a página atual
-current_page = None
-
-# Criar botões em colunas
-col_count = len(pages)
-cols = st.columns(col_count)
-
-# Criar botões para as páginas
-for i, page_name in enumerate(pages.keys()):
-    with cols[i]:
-        if st.button(page_name):
-            current_page = page_name  # Define a página atual
-
-# Carregar a página selecionada
-if current_page:
-    page = pages[current_page]
-    page()  # Chama a função 'app()' da página selecionada
-else:
-    st.write("Por favor, selecione uma página.")
