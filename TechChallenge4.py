a44e10d4087b5f77dac45c692868f1013997078d
DanielaSSpina <46855272+DanielaSSpina@users.noreply.github.com>
2024-10-27 21:03:01 -0300

    Update TechChallenge4.py

--- TechChallenge4.py
+++ TechChallenge4.py
@@ -1,11 +1,10 @@
 import streamlit as st
-from pages.Introdução.py import app as Introdução_app
-from pages.Metodologia.py import app as Metodologia_app
-from pages.Análise.py import app as Análise_app
-from pages.Previsão.py import app as Previsão_app
-from pages.Referências.py import app as Referências_app
+from pages.Introdução import app as Introdução_app
+from pages.Metodologia import app as Metodologia_app
+from pages.Análise import app as Análise_app
+from pages.Previsão import app as Previsão_app
+from pages.Referências import app as Referências_app
 
-# Título da aplicação
 st.title('Tech Challenge 4 | Fiapp')
 
 # Dicionário com as páginas
@@ -17,23 +16,20 @@ pages = {
     "Referências": Referências_app
 }
 
-# Inicializa a variável para a página atual
 current_page = None
 
-# Cria colunas para os botões
+# Criar botões em colunas
 col_count = len(pages)
 cols = st.columns(col_count)
 
-# Criar botões em colunas
 for i, page_name in enumerate(pages.keys()):
     with cols[i]:
-        if st.button(page_name):  # Cria um botão para cada página
-            current_page = page_name  # Define a página atual como a página do botão clicado
+        if st.button(page_name):
+            current_page = page_name
 
 # Carregar a página selecionada
 if current_page:
     page = pages[current_page]
     page()  # Chama a função 'app()' da página selecionada
 else:
-    # Carrega uma página padrão se nenhum botão for clicado
     st.write("Por favor, selecione uma página.")
