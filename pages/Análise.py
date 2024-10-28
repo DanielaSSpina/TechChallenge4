62e7076fc85d3d2d340a26ea1986ad5304431528
DanielaSSpina <46855272+DanielaSSpina@users.noreply.github.com>
2024-10-27 21:05:55 -0300

    Update Análise.py

--- pages/Análise.py
+++ pages/Análise.py
@@ -3,4 +3,5 @@ import streamlit as st
 
 def app():
     st.title("Análise")
-    st.title("teste")
+    st.write("Esta é a página contendo análises.")
+    st.write("Teste para verificar se a página carrega corretamente.")
