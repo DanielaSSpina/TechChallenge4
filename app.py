7cd2de491ed995f0e9ed23988d2ebacf5fa3e68e
DanielaSSpina <46855272+DanielaSSpina@users.noreply.github.com>
2024-11-03 18:05:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.markdown("<h3 style='color:#1C2382;'>Tech Challenge 4 | Fiap</h3>", unsafe_allow_html=True)
+st.markdown("<h3 style='color:#264CAC;'>Tech Challenge 4 | Fiap</h3>", unsafe_allow_html=True)
 st.markdown("<h3 style='color:#6C778A;'>Introdução</h3>", unsafe_allow_html=True)
 
 st.write('''Este trabalho tem como foco apresentar a análise do preço do petróleo brent, trazendo quatro acontecimentos que auxiliem a explicação das variação do preço e podem variar desde fatores geopolíticos até avanços tecnológicos. 
