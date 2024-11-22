c6245ad69c244f3a9836bc5184f5a77b3537dacc
DanielaSSpina <46855272+DanielaSSpina@users.noreply.github.com>
2024-11-21 23:05:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,10 @@ if pagina == "Introdução":
 Traremos além dos acontecimentos, uma análise por meio de um dashboard dinâmico dos dados, previsão analisada e MVP.''')
 
     st.markdown("<h3 style='color:#264CAC;'>Ferramentas utilizadas</h3>", unsafe_allow_html=True)
-    st.write('Para a realização deste trabalho, foi utilizado as seguintes ferramentas: ...')
+    st.write(''''Para a realização deste trabalho, foi utilizado as seguintes ferramentas:
+Python: utilizado para toda a tratativa inicial das bases como organização das colunas, remoção de espaços e duplicidades e valores nulo, assim como para a realização da previsão dos preços do petróleo por meio do modelo de machine learning PROPHET.
+PowerBI: utilizado para a criação de um dashboard interativo compilando as informações disponíveis do preço do petróleo, previsão e acontecimentos que influenciam na explicação da variação do preço.
+Streamlit: utilizado para desenvolvimento do MVP (Minimum Viable Product, ou Produto Mínimo Viável) e disponibilização das etapas e informações do projeto. ''''')
 
 elif pagina == "Metodologia":
     st.markdown("<h3 style='color:#264CAC;'>Metodologia</h3>", unsafe_allow_html=True)
